a45808fcc5e9947ffc73fda8f6d9cbeb9bcd5f2e
kota <kota0330.hori@gmail.com>
2020-02-11 04:26:43 +0900

    first commit

---
+++ myamplifyproject/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <li><router-link to="/ai">AIアプリ</router-link></li>
+      <li><router-link to="/chat">チャットアプリ</router-link></li>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style src="./assets/style.css" />
