d1b397ce0bdefac46a613e6be5fba227ea76a4cd
kota <kota0330.hori@gmail.com>
2020-02-11 04:36:03 +0900

    add auth

--- myamplifyproject/src/App.vue
+++ myamplifyproject/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <li><router-link to="/ai">AIアプリ</router-link></li>
-      <li><router-link to="/chat">チャットアプリ</router-link></li>
-      <router-view />
+      <amplify-authenticator />
+      <div id="nav">
+        <li><router-link to="/ai">AIアプリ</router-link></li>
+        <li><router-link to="/chat">チャットアプリ</router-link></li>
     </div>
+    <router-view/>
   </div>
 </template>
 
